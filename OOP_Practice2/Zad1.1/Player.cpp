b1e4409ab19b88cb4ed2da6acfe7c7f6ad8dc081
Lachezar Kolev <lachezar.kolev.highschool@svetlina.net>
2025-05-10 18:23:52 +0300

    started preparation for K2

---
+++ OOP_Practice2/Zad1.1/Player.cpp
@@ -0,0 +1,107 @@
+﻿#include "Player.h"
+#include <cmath>
+#include <stdexcept>
+
+bool Player::Point::areAdjacent(const Point& other) const
+{
+	return abs(x - other.x) == 1 && abs(y - other.y) == 1;
+}
+
+void Player::copyDynamic(const Player& other)
+{
+	if (!other.name)
+		throw std::runtime_error("cannot copy empty name");
+
+	name = new char[strlen(other.name) + 1];
+
+	strcpy(name, other.name);
+}
+
+void Player::stealData(Player&& other)
+{
+	health = other.health;
+	name = other.name;
+	position = other.position;
+	weapon = other.weapon;
+	attackDamage = other.attackDamage;
+
+	other.name = nullptr;
+}
+
+void Player::swapData(Player& other) noexcept
+{
+	std::swap(health, other.health);
+	std::swap(name, other.name);
+	std::swap(position, other.position);
+	std::swap(weapon, other.weapon);
+	std::swap(attackDamage, other.attackDamage);
+}
+
+void Player::free()
+{
+	delete[] name;
+	name = nullptr;
+}
+
+void Player::move(int offsetX, int offsetY)
+{
+	position.x += offsetX;
+	position.y += offsetY;
+}
+
+void Player::handleAttack(const Player& attacker)
+{
+	health -= attacker.getAttackPower();
+}
+
+void Player::attack(Player& attacked)
+{
+	attacked.health -= attackDamage;
+}
+
+unsigned Player::getAttackPower() const
+{
+	return attackDamage;
+}
+
+void Player::setAttackPower(unsigned attackPower)
+{
+	if (attackPower < 0)
+		throw std::invalid_argument("attack power cannot be < 0");
+	this->attackDamage = attackPower;
+}
+
+Player::Player(const Player& other)
+{
+	try
+	{
+		copyDynamic(other);
+		setAttackPower(other.attackDamage);
+		health = other.health;
+		weapon = other.weapon;
+		position = other.position;
+	}
+	catch (...)
+	{
+		free();
+		throw;
+	}
+}
+
+Player::Player(Player&& other)
+{
+	if (other.attackDamage < 0)
+		throw std::invalid_argument("attack cannot be < 0");
+
+	stealData(std::move(other));
+}
+
+Player& Player::operator=(const Player& other)
+{
+	if (this != &other)
+	{
+		// ударихме на камък
+		Player copy(other);
+
+	}
+}
---
+++ OOP_Practice2/Zad1.1/Player.h
@@ -0,0 +1,53 @@
+#pragma once
+class Player
+{
+	unsigned health = 1;
+	char* name = nullptr;
+
+	struct Point {
+		int x;
+		int y;
+		Point() :x(0), y(0) {};
+		Point(int x, int y) :x(x), y(y) {};
+		bool areAdjacent(const Point& other) const;
+	} position;
+
+	enum class Weapons
+	{
+		SWORD,
+		WAND,
+		STAFF,
+		AXE
+	} weapon;
+
+	unsigned attackDamage = (unsigned)weapon;
+
+	void copyDynamic(const Player& other);
+
+	void stealData(Player&& other) noexcept;
+
+	void swapData(Player& other) noexcept;
+
+	void free();
+
+public:
+
+	void move(int offsetX, int offsetY);
+
+	virtual void handleAttack(const Player& attacker) = 0;
+
+	virtual void attack(Player& attacked) = 0;
+
+	unsigned getAttackPower() const;
+
+	void setAttackPower(unsigned attackPower);
+
+	Player() = default;
+	Player(const Player& other);
+	Player(Player&& other);
+
+	Player& operator=(const Player& other);
+	Player& operator=(Player&& other);
+
+	virtual ~Player();
+};
